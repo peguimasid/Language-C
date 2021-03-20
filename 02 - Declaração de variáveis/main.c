311b8835b1bdafb22590880359615464b546ea4e
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-20 20:34:35 -0300

    Aula 02 - Declaracao de variaveis

---
+++ 02 - Declaração de variáveis/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  // tipo nome;
+
+  // tipos:
+  // char -> Guardar caractéres ou numero de -128 até 127
+  // int -> Valores inteiros
+  // float -> Numeros reais
+  // double -> Numero real com maior precisão
+
+  char letter = 'a';
+  char number = 10;
+
+  int num = 10;
+  float n = 5.25;
+  double d = 5.0e3;
+
+  return 0;
+}
