069f3be1513d207f60f83116ff8bea78a824efcf
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-24 17:03:54 -0300

    Aula 08 - Comentarios

---
+++ 08 - Comentários/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x = 10;
+
+  /*
+   int w = 11;
+   int x = 12;
+   int y = 13;
+   */
+
+  // x = x + 6;
+
+  printf("%d", x);
+
+  return 0;
+}
