29f4c09007a1bffe5780e21b4879fbcad44595e3
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-25 17:23:01 -0300

    Aula 10 - Atribuicao simplificada

---
+++ 10 - Atribuição simplificada/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // Sem atribuição simplificada
+  // int x = 10;
+  // int y = 10;
+
+  // x = x + y;
+
+  // Com atribuição simplificada
+  int x = 12;
+  int y = 10;
+
+  // x *= y; -> x = x * y
+  // x += y; -> x = x + y
+  // x /= y; -> x = x / y
+  // x %= y; -> x = x % y
+
+  printf("%d", x);
+
+  return 0;
+}
