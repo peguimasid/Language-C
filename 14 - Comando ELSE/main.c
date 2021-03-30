332ac734c7292c5dcd066da5fec290faf153fc69
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-30 16:14:00 -0300

    Aula 14 - Comando ELSE

---
+++ 14 - Comando ELSE/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int x = -1;
+
+  if (x > 0) {
+    printf("O valor é positivo");
+  } else {
+    printf("O valor é negativo");
+  }
+
+  return 0;
+}
